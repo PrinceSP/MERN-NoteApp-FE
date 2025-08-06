30a12707a95910f345502d01129d9c0915b84ba0
PrinceSP <princepasombaran@gmail.com>
2025-08-06 17:35:24 +0700

    feat: add glow effect

--- src/App.tsx
+++ src/App.tsx
@@ -2,19 +2,18 @@ import { Route, Routes } from "react-router"
 import Home from "./pages/home"
 import CreateNote from "./pages/createNote"
 import NoteDetails from "./pages/noteDetails"
-import toast from "react-hot-toast"
 
 function App() {
 
   return (
-    <>
-      <button onClick={()=>toast.success("success hahaha")}>test</button>
+    <main className="relative h-full w-full">
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_60%,#00FF9D40_100%)]"/>
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/create" element={<CreateNote/>}/>
-        <Route path="/details" element={<NoteDetails/>}/>
+        <Route path="/note-details" element={<NoteDetails/>}/>
       </Routes>
-    </>
+    </main>
   )
 }
 
