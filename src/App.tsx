ca54e94a75f9a3ff4ce96f8bcd30970fcc0a7aa6
PrinceSP <princepasombaran@gmail.com>
2025-08-07 17:53:15 +0700

    fix: fix routing params

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home/>}/>
         <Route path="/create" element={<CreateNote/>}/>
-        <Route path="/note-details" element={<NoteDetails/>}/>
+        <Route path="/note/:id" element={<NoteDetails/>}/>
       </Routes>
     </main>
   )
